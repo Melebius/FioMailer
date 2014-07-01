c82623e04e112d6e6ab33d49dd2111e69b71ff97
Miroslav Mateju <melebius@gmail.com>
2014-07-01 20:08:35 +0200

    NEW fio-mailer.class.php
    Zastřešující třída pro automatické stažení dat z banky a rozeslání mailů.
    
    Metoda sendAlertDigest() zatím odešle mail, i když žádná transakce neproběhla.
    
    Signed-off-by: Miroslav Mateju <melebius@gmail.com>

---
+++ fio-mailer.class.php
@@ -0,0 +1,171 @@
+<?php
+require_once('transact-csv-rdr.class.php');
+require_once('phpmailer/class.phpmailer.php');
+require_once('phpmailer/class.smtp.php');
+require_once('phpmailer/language/phpmailer.lang-cz.php');
+
+/**
+ * @brief Zasílač upozornění na transakce ve FIO.
+ *
+ * Třída provádí načtení seznamu posledních transakcí z FIO
+ * a zašle mailové upozornění na transakce splňující zadané kritérium.
+ *
+ * Používá knihovnu PHPMailer: https://github.com/PHPMailer/PHPMailer
+ *
+ * @author Miroslav Matějů @<melebius@gmail.com@>
+ */
+class FioMailer
+{
+  // -------------------------- PUBLIC -----------------------------------
+  /**
+   * Vytvoření objektu se zadaným autentizačním tokenem FIO.
+   * @param string $_token autentizační token FIO
+   */
+  public function __construct($_token)
+  {
+    $this->token = $_token;
+  }
+
+  /**
+   * Načte z banky historii transakcí od posledního zavolání této funkce.
+   */
+  public function readHistory()
+  {
+    // stažení dat z banky
+    $csv = file(self::URL_BASE . $this->token . '/transactions.csv');
+    $this->csv_reader = new TransactCsvReader($csv);
+    return true;
+  }
+
+  /**
+   * Nastavení filtru transakcí.
+   * Viz informace u třídy TransactCsvReader.
+   * @param string $_key název sloupce v tabulce transakcí
+   * @param string $_value žádaný začátek hodnoty v daném sloupci
+   * @return bool úspěšnost nastavení filtru
+   */
+  public function setFilter($_key, $_value)
+  {
+    if(!is_null($this->csv_reader))
+    {
+      return $this->csv_reader->setFilter($_key, $_value);
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Zaslání upozornění na všechny vybrané transakce v jedné zprávě.
+   * @param string $_to e-mailová adresa příjemce
+   * @param string $_subj_start začátek předmětu e-mailu
+   * @return bool úspěšnost odeslání
+   * @todo Odesílá i prázdný seznam!
+   */
+  public function sendAlertDigest($_to, $_subj_start)
+  {
+    $subject = $_subj_start . ' – upozornění na transakce';
+
+    $body = "V bance proběhly následující sledované transakce:\n".
+            "=================================================\n\n";
+
+    while($this->csv_reader->findNext())
+    {
+      $body .= $this->printTransaction().
+               "\n=================================================\n\n";
+    }
+
+    $body .= "Odeslal FioMailer Akademických týdnů.\n";
+
+    return $this->sendMail($_to, $subject, $body);
+  }
+
+  // ------------------------- PRIVATE -----------------------------------
+  private $csv_reader = NULL; ///< čtečka transakcí
+  private $token;             ///< autentizační token banky
+
+  /// stálá část URL pro komunikaci s bankou
+  const URL_BASE = 'https://www.fio.cz/ib_api/rest/last/';
+
+  /**
+   * Vypíše údaje o právě vybrané transakci.
+   * @return string řetězec obsahující na jednotlivých řádcích údaje o transakci
+   */
+  private function printTransaction()
+  {
+    return
+      $this->printValue('ID pohybu').
+      $this->printValue('Datum').
+      $this->printValue('Objem').
+      $this->printValue('Měna').
+      $this->printValue('Protiúčet').
+      $this->printValue('Název protiúčtu').
+      $this->printValue('Kód banky').
+      $this->printValue('Název banky').
+      $this->printValue('KS').
+      $this->printValue('VS').
+      $this->printValue('SS').
+      $this->printValue('Uživatelská identifikace').
+      $this->printValue('Zpráva pro příjemce').
+      $this->printValue('Typ').
+      $this->printValue('Provedl').
+      $this->printValue('Upřesnění').
+      $this->printValue('Komentář').
+      $this->printValue('BIC').
+      $this->printValue('ID pokynu');
+  }
+
+  /**
+   * Výpis jednoho údaje jedné transakce.
+   * @param unknown $key
+   * @return string řádek ve formátu $key: hodnota\\n
+   */
+  private function printValue($key)
+  {
+    return $key.': '.$this->csv_reader->readTransactData($key)."\n";
+  }
+
+  /**
+   * Odeslání e-mailu pomocí knihovny PHPMailer.
+   * @param string $_to
+   * @param string $_subject
+   * @param string $_body
+   * @return bool úspěšnost odeslání
+   */
+  private function sendMail($_to, $_subject, $_body)
+  {
+    $mail = new PHPMailer();
+
+    // přihlášení k serveru
+    $mail->IsSMTP();  // volba metody odeslání
+    $mail->SMTPAuth    = true;
+    $mail->SMTPSecure  = 'ssl';
+    $mail->Host        = 'smtp.gmail.com';
+    $mail->Port        = 465;
+    $mail->Username    = 'DOPLNIT@ADRESU.ODESILATELE';
+    $mail->Password    = '---DOPLNIT HESLO---';
+
+    // hlavička zprávy (stálá data)
+    $mail->From        = $mail->Username;     // adresa = přihlašovací jméno
+    $mail->FromName    = 'DOPLNIT JMENO ODESILATELE';
+    $mail->SetLanguage('cz');
+    $mail->CharSet     = 'utf-8';
+    $mail->ContentType = 'text/plain';
+    $mail->IsHTML(false);
+
+    // obsah zprávy (proměnná data)
+    $mail->AddAddress($_to);
+    $mail->Subject     = $_subject;
+    $mail->Body        = $_body;
+
+    // vlastní odeslání
+    $result = $mail->Send();
+    if(!$result)
+    {
+      echo $mail->ErrorInfo;
+    }
+    return $result;
+  }
+}
+?>
